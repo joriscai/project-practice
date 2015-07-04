545339546fe4269113b6f6f10764da13745cb4b6
cxm <707039333@qq.com>
2015-07-04 15:26:56 +0800

    增加快捷访问

---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class UserController extends Controller{
+	public function regedit(){
+		$this->show('your mother boom');
+	}
+	
+	public function login(){
+		$this->show('越杨上传了吗？');
+	}
+}
