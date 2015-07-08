0d70494c1860a2e46dea7c5fdb3d5693aec2834d
cxm <707039333@qq.com>
2015-07-08 10:48:34 +0800

    完成购物车后台和完成商品显示页面

--- Public/JS/ShoppingCart/ShoppingCart.js
+++ Public/JS/ShoppingCart/ShoppingCart.js
@@ -138,10 +138,40 @@
 //
 
 $(function(){
-	$(".num_ad").click(
+	function num_op(op,n,id){
+			var p=$('#price_'+id).val();
+
+			
+			//小计和总计需要变化
+			$.ajax({
+				type:"post",
+				url:op_num_path,
+				async:true,
+				data:{
+					op:op,
+					id:id,
+					num:n
+				},
+				success:function(msg){
+					if(parseInt(msg)>=0){
+						//g_sum
+						var g='.g_sum_'+id;
+						$(g).html(n*p);//设置小计
+						$('.a_price').html('¥'+msg);
+					}else{
+						alert(msg);
+					}
+				}
+			});
+	}
+	$(".num_ad").click(//商品数量增加
 		function()
 		{
-			$(this).prev().val(parseInt($(this).prev().val())+1);
+			var id=$(this).attr('value');
+			var v=$(this).prev().val(),//获取数量
+			n=parseInt(v)+1;//数量加1
+			$(this).prev().val(n);
+			num_op('a',n,id);
 		}
 	)
 	$(".num_re").click(
@@ -155,22 +185,40 @@ $(function(){
 			{
 				$(this).next().val(parseInt($(this).next().val())-1);
 			}
+			var id=$(this).attr('value');
+			num_op('r',$(this).next().val(),id);
 		}
 	)
 	$(".shopid").keyup(
 		function()
 		{
+			var num=$(this).val(),
+			id=$(this).attr('shopid');
+			if(num==''){
+				return;
+			}
+			
+			
 			var regx=/^[0-9]{1,20}$/;
-			if(!regx.exec($(this).val()))
+			if(!regx.exec(num))
 			{
 				$(this).val(1);
 			}
+			num_op('a',num,id);
+			
+			
+			
 		}
 	)
 	$(".chec").click(
 		function()
 		{
-			if($(this).get(0).checked)
+			//是否选中
+			var c=$(this).get(0).checked==false?0:1,
+			id=$(this).attr('shopid');
+			
+			
+			if(c)
 			{
 				$(this).parent().parent().removeClass("mid_main2");
 				$(this).parent().parent().addClass("mid_main1");
@@ -180,8 +228,55 @@ $(function(){
 				$(this).parent().parent().removeClass("mid_main1");
 				$(this).parent().parent().addClass("mid_main2");
 			}
+			//后台操作
+			$.ajax({
+				type:"post",
+				url:op_s_path,
+				async:true,
+				data:{
+					c:c,
+					id:id
+				},
+				success:function(msg){
+					
+					if(typeof(msg)=='object'){
+						$('.a_price').html('¥'+msg.ap);
+						$('.a_count').html(msg.acount);
+					}else{
+						alert(msg)
+					}
+					
+				}
+			});
+			
 			
 		}
 	)
+	
+	$('.good_d').click(function(){
+		id=$(this).attr('value');
+		$.ajax({
+			type:"post",
+			url:op_del_path,
+			async:true,
+			data:{
+				id:id
+			},
+			success:function(msg){
+				if(msg==2){
+					window.location=home_path;
+				}else if(msg==0){
+					alert('操作失败');
+				}else if(typeof(msg)=='object'){
+					$('.a_price').html('¥'+msg.ap);
+					$('.a_count').html(msg.acount);
+					$('#all_'+id).remove();
+				}else{
+					alert(msg);
+				}
+			}
+		});
+		
+	})
 })
 
