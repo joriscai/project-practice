e4e0ca3b5c021b8cc6d1c4adabe4390313e9fb89
cxm <707039333@qq.com>
2015-07-09 20:04:48 +0800

    合并横排 竖排页面 关于我们页面 修复footer一个居中的bug

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,7 +2,36 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+	//首页
     public function index(){
         $this->display('Index/index');
     }
+	
+	//横排导航页面
+	public function market(){
+		$this->display('Index/market');
+	}
+	
+	//竖排导航页面
+	public function cate(){
+		$this->display('Index/cate');
+	}
+	
+	//关于我们页面
+	public function aboutus(){
+		$this->display('About/index');
+	}
+	
+	public function cul(){
+		$this->display('About/cul');
+	}
+	public function mod(){
+		$this->display('About/mod');
+	}
+	public function comn(){
+		$this->display('About/comn');
+	}
+	public function soc(){
+		$this->display('About/soc');
+	}
 }
